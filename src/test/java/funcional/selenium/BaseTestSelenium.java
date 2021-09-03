a9f0fc94fee25445e96fd383c0801b6b09506d00
Leonardo Santos Martins <leo_smartins@outlook.com>
2021-09-03 11:22:55 -0300

    Ajuste BaseTestSelenium e remoção tasks personalizadas

--- src/test/java/funcional/selenium/BaseTestSelenium.java
+++ src/test/java/funcional/selenium/BaseTestSelenium.java
@@ -5,6 +5,8 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 
+import static java.util.concurrent.TimeUnit.SECONDS;
+
 public class BaseTestSelenium {
 
     WebDriver driver = null;
@@ -12,11 +14,14 @@ public class BaseTestSelenium {
     @BeforeClass
     public WebDriver before() {
         System.setProperty("webdriver.chrome.driver", "src/test/resources/chromedriver.exe");
-        return driver = new ChromeDriver();
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(30, SECONDS);
+        return driver;
     }
 
     @AfterClass
-    public void after(){
+    public void after() {
         driver.quit();
     }
 
